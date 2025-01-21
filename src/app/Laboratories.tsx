fe3fc680f2d9d9a0c0f19ba8d470088cce59ea5d
Tiago Vargas <tiago.vargaspo@proton.me>
2025-01-21 16:34:07 -0300

    stub lab list

--- src/app/Laboratories.tsx
+++ src/app/Laboratories.tsx
@@ -1,10 +1,55 @@
-// import "./Laboratories.css";
+import "./Laboratories.css";
+
+interface CardProps {
+	title: string;
+	description: string;
+	link: string;
+}
 
 function Laboratories() {
+	const laboratories = [
+		{ title: "LaTIM", description: "Laboratório de Tecnologias de Informação em Medicina", link: "/laboratorios/latim" },
+		{ title: "LabVICIA", description: "Laboratório de Visão Computacional e Imagem Aplicada", link: "/laboratorios/labvicia" },
+		{ title: "LASIC", description: "Laboratório de Sistemas Integráveis e Computação", link: "/laboratorios/lasic" },
+		{ title: "LAESE", description: "Laboratório de Engenharia de Software e Sistemas", link: "/laboratorios/laese" }
+	];
+
 	return (
-		<div className="Laboratories">
-			<h1>Laboratórios</h1>
+		<div>
+			<header>
+				<h1>Laboratórios</h1>
+				<p>Veja nossos laboratórios e requisite nossos serviços</p>
+			</header>
+
+			<main>
+				<h2>Nossos Laboratórios</h2>
+				<p>Selecione um laboratório para ver mais detalhes:</p>
+				<ul>
+					{
+						laboratories.map((lab, index) => (
+							<li key={index}>
+								<Card
+									title={lab.title}
+									description={lab.description}
+									link={lab.link}
+								/>
+							</li>
+						))
+					}
+				</ul>
+			</main>
 		</div>
+	);
+}
+
+function Card(props: { title: string; description: string; link: string;}) {
+	return (
+		<a href={props.link}>
+			<div className="Card">
+				<h3>{props.title}</h3>
+				<p>{props.description}</p>
+			</div>
+		</a>
 	);
 }
 
