2dc4ede4a565bbf0cec80057821fef2ee483a76d
Tiago Vargas <tiago.vargaspo@proton.me>
2025-01-21 13:30:10 -0300

    populate sections

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
@@ -14,20 +12,40 @@ function App() {
 						<a className="item link" href="#contact">Contato</a>
 				</nav>
 			</div>
+
 			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.tsx</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
+				<h1>Vitrine Tecnológica do IFCE</h1>
+				<p>Veja nossos laboratórios disponíveis e requisite nossos serviços</p>
 			</header>
+
+			<main>
+				<section>
+					<h2>Laboratórios</h2>
+					<p>Conheça nossos laboratórios disponíveis</p>
+					<a href="/labs">Conferir</a>
+				</section>
+				<section>
+					<h2>Sobre</h2>
+					<p>Conheça mais sobre a Vitrine Tecnológica</p>
+				</section>
+				<section>
+					<h2>Contato</h2>
+					<p>Entre em contato conosco</p>
+
+					<label htmlFor="telephone">Telefone</label>
+					<p id="telephone">(85) 3307-3665</p>
+
+					<label htmlFor="email">Email</label>
+					<p id="email">blablabla@exemplo.com.br</p>
+
+					<label htmlFor="address">Endereço</label>
+					<address id="address">IFCE - Bloco 3 (Ciência da Computação)<br />
+						Av. Parque Central, 1315<br />
+						Distrito Industrial I<br />
+						Maracanaú - CE, 61939-140
+					</address>
+				</section>
+			</main>
 		</div>
 	);
 }
